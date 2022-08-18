1b426c0f43bd69199c5f3fc80a00163195625d86
Jon Robison <narfman0@gmail.com>
2022-08-18 16:32:54 -0400

    fix multiprocessing pyinstaller crap

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from multiprocessing import freeze_support
 from smb3_eh_manip.main import main
 
 # this is intended for pyinstaller only!
 if __name__ == "__main__":
+    freeze_support()
     main()
