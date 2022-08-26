4319c51b743adce0031e3a8cd2df2fc17a80f7ef
Jon Robison <narfman0@gmail.com>
2022-08-25 21:04:03 -0400

    reuse the eh video with great offset. nonsense 25mb file.

--- smb3_eh_manip/computers/two_one_computer.py
+++ smb3_eh_manip/computers/two_one_computer.py
@@ -6,8 +6,12 @@ class TwoOneComputer(OpencvComputer):
     def __init__(self):
         super().__init__(
             "twoonevideo",
-            settings.get("two_one_video_path", fallback="data/two_one/video.mp4"),
+            settings.get("two_one_video_path", fallback="data/eh/video.avi"),
             settings.get(
                 "two_one_start_frame_image_path", fallback="data/two_one/trigger.png"
+            ),
+            video_offset_frames=16680,
+            start_frame_image_region=settings.get_config_region(
+                "two_one_start_frame_image_region"
             ),
         )
