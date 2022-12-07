627e015cc25e34cf2063ced36aca8dbc2179554a
Jon Robison <narfman0@gmail.com>
2022-12-07 11:31:53 -0500

    increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="smb3-eh-manip",
-    version="0.20.0",
+    version="1.0.0",
     description=("Ingest video data to render smb3 eh manip stimuli"),
     classifiers=[
         "Development Status :: 4 - Beta",
