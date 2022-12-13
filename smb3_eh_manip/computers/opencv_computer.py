2dd2e63cb2296bac74e0f3577f9a1aa1f3cc8c6a
Jon Robison <narfman0@gmail.com>
2022-12-13 10:59:01 -0500

    fix logical issue with retrospy and serial servers

--- smb3_eh_manip/computers/opencv_computer.py
+++ smb3_eh_manip/computers/opencv_computer.py
@@ -223,10 +223,12 @@ class OpencvComputer:
             lag_frame_detect_start = time.time()
             if self.auto_detect_lag_frames_serial:
                 self.serial_server.tick(self.current_frame)
+                self.lag_frames = self.serial_server.lag_frames_observed
+                self.load_frames = self.serial_server.load_frames_observed
             elif self.auto_detect_lag_frames_retrospy:
                 self.retrospy_server.tick(self.current_frame)
-            self.lag_frames = self.retrospy_server.lag_frames_observed
-            self.load_frames = self.retrospy_server.load_frames_observed
+                self.lag_frames = self.retrospy_server.lag_frames_observed
+                self.load_frames = self.retrospy_server.load_frames_observed
             detect_duration = time.time() - lag_frame_detect_start
             if self.playing and detect_duration > 0.002:
                 logging.info(f"Took {detect_duration}s detecting lag frames")
