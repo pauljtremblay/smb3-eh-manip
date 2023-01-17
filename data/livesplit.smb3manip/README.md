65527515158f1e8dbebd5c42bf4b418c3e26cacb
Jon Robison <narfman0@gmail.com>
2023-01-17 16:39:45 -0500

    move livesplit component to separate repo

--- data/livesplit.smb3manip/README.md
+++
@@ -1,9 +0,0 @@
-## LiveSplit.Smb3Manip
-A Simple counter component for LiveSplit. 
-
-## Packages / Requirements
-
-- If you plan on building this solution yourself, you may need to add references to the following dlls (the included versions may be outdated), all of which are distributed with LiveSplit: [http://livesplit.org/](http://livesplit.org/ "Livesplit Home").
-	+ LiveSplit.Core.dll
-	+ UpdateManger.dll
-	+ WinformsColor.dll
--- smb3_eh_manip/app/servers/livesplit_smb3manip.py
+++ smb3_eh_manip/app/servers/livesplit_smb3manip.py
@@ -7,6 +7,15 @@ from enum import Enum
 from smb3_eh_manip.app.state import State
 from smb3_eh_manip.util import settings
 
+"""
+When using the manip tool, theres a big stinkin' UI that is helpful to put in
+OBS, but too big imo. A livesplit component has been created:
+https://github.com/narfman0/LiveSplit.Smb3Manip/releases
+
+This module emits packets the component needs to render, like when we reset
+and when lag frames are encountered.
+"""
+
 EPOCH_OFFSET = 1673989120228  # TODO i dont want to pass 8 bytes so subtracting this ehre and adding on other side
 PORT = settings.get_int("livesplit_smb3manip_port", fallback=25345)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -33,22 +42,22 @@ class LivesplitSmb3Manip:
 
     def start_playing(self, offset_start_time: int):
         payload = (offset_start_time - EPOCH_OFFSET).to_bytes(4, sys.byteorder)
-        thread = Thread(
-            target=send_packet, args=(PacketType.START_TIME_UPDATED, payload)
+        self.emit_message(PacketType.START_TIME_UPDATED, payload)
+        logging.debug(
+            f"Emitted livesplit.smb3manip packet offset_start_time {offset_start_time}"
         )
-        thread.start()
-        thread.join()
 
     def tick(self, state: State, current_frame: int):
         lag_frames = state.total_observed_lag_frames + state.total_observed_load_frames
         if self.last_lag_frames != lag_frames:
             self.last_lag_frames = lag_frames
             payload = lag_frames.to_bytes(2, sys.byteorder)
-            thread = Thread(
-                target=send_packet, args=(PacketType.LAG_FRAMES_UPDATED, payload)
-            )
-            thread.start()
-            thread.join()
+            self.emit_message(PacketType.LAG_FRAMES_UPDATED, payload)
             logging.debug(
                 f"Emitted livesplit.smb3manip packet at frame {current_frame}"
             )
+
+    def emit_message(self, packet_type: PacketType, payload: bytes):
+        thread = Thread(target=send_packet, args=(packet_type, payload))
+        thread.start()
+        thread.join()
