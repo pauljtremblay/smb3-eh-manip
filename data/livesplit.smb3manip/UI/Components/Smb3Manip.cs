bff6eb56e106730361196d4bf6971afd1df88b5d
Jon Robison <narfman0@gmail.com>
2023-01-17 12:52:21 -0500

    fix manip component. works.

--- data/livesplit.smb3manip/UI/Components/Smb3Manip.cs
+++ data/livesplit.smb3manip/UI/Components/Smb3Manip.cs
@@ -1,25 +1,28 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Net;
 using System.Net.Sockets;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace LiveSplit.UI.Components
 {
     public class Smb3Manip
     {
         private const int DEFAULT_PORT = 25345;
+        private const string LOG_PATH = "Components/smb3manip.log";
         private int port = DEFAULT_PORT;
-        private string currentStr = "nil";
+        protected static Thread UpdateThread { get; private set; }
+        public static volatile uint currentFrame = 0, lagFrames = 0;
 
         public Smb3Manip(int port = DEFAULT_PORT)
         {
             this.port = port;
-            SetCurrentStr(0, 0);
-            StartUpdateThread();
-        }
-
-        public void Reset()
-        {
-            SetCurrentStr(0, 0);
+            if (UpdateThread == null)
+            {
+                UpdateThread = new Thread(new ThreadStart(ReadCommands));
+                //UpdateThread.IsBackground = true;
+                UpdateThread.Start();
+            }
         }
 
         public void SetPort(int port)
@@ -27,33 +30,30 @@ namespace LiveSplit.UI.Components
             this.port = port;
         }
 
-        public void SetCurrentStr(int currentFrame, int lagFrames)
-        {
-            currentStr = "Frame: " + currentFrame + " Lag Frames: " + lagFrames;
-        }
-
         public string GetCurrentStr()
         {
-            return currentStr;
+            return "Frame: " + currentFrame + " Lag: " + lagFrames;
         }
 
-        private void StartUpdateThread()
+        public void ReadCommands()
         {
-            Task.Run(async () =>
+            try
             {
-                File.AppendAllText("smb3manip.log", "Listening on port " + port);
-                using (var udpClient = new UdpClient(port))
+                File.AppendAllText(LOG_PATH, "Listening on port " + port + "\n");
+                UdpClient udpClient = new UdpClient();
+                udpClient.Client.Bind(new IPEndPoint(0, port));
+                var from = new IPEndPoint(0, 0);
+                while (true)
                 {
-                    while (true)
-                    {
-                        var receivedResult = await udpClient.ReceiveAsync();
-                        int currentFrame = System.BitConverter.ToInt32(receivedResult.Buffer, 0);
-                        int lagFrames = System.BitConverter.ToInt32(receivedResult.Buffer, 4);
-                        File.AppendAllText("smb3manip.log", "Received " + currentFrame + " | " + lagFrames);
-                        SetCurrentStr(currentFrame, lagFrames);
-                    }
+                    var receivedResult = udpClient.Receive(ref from);
+                    currentFrame = BitConverter.ToUInt32(receivedResult, 0);
+                    lagFrames = BitConverter.ToUInt16(receivedResult, 4);
+                    File.AppendAllText(LOG_PATH, "Received " + currentFrame + " | " + lagFrames + "\n");
                 }
-            });
+            }
+            catch(Exception e) {
+                File.AppendAllText(LOG_PATH, "Exception " + e + "\n");
+            }
         }
     }
 }
