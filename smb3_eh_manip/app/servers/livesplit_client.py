5d6f83a6d7b6684265807dea45e7a80ddddaf4e9
Jon Robison <narfman0@gmail.com>
2023-02-02 12:24:24 -0500

    fix livesplit client to use named pipe. remove extranneous garbage.

--- smb3_eh_manip/app/servers/livesplit_client.py
+++ smb3_eh_manip/app/servers/livesplit_client.py
@@ -7,14 +7,9 @@ import win32file, win32pipe
 from smb3_eh_manip.util import events, settings
 from smb3_eh_manip.util.logging import initialize_logging
 
-
-CONNECTION_REFUSED_ERROR = (
-    "Livesplit ConnectionRefusedError (is the server running?), waiting..."
-)
 LIVESPLIT_REQUEST_FREQUENCY = settings.get_float(
     "livesplit_request_frequency", fallback=0.25
 )
-LIVESPLIT_SERVER_PORT = settings.get_int("livesplit_server_port", fallback=16834)
 
 
 def client_process(split_index_value: Value):
@@ -23,20 +18,25 @@ def client_process(split_index_value: Value):
         file_log_level="DEBUG",
         filename="livesplit_client.log",
     )
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connected = False
-    while not connected:
-        try:
-            s.connect(("localhost", LIVESPLIT_SERVER_PORT))
-            logging.info(f"Livesplit client socket connected")
-            connected = True
-        except ConnectionRefusedError:
-            logging.warn(CONNECTION_REFUSED_ERROR)
-            time.sleep(1)
+    handle = win32file.CreateFile(
+        r"\\.\pipe\LiveSplit",
+        win32file.GENERIC_READ | win32file.GENERIC_WRITE,
+        0,
+        None,
+        win32file.OPEN_EXISTING,
+        win32file.FILE_ATTRIBUTE_NORMAL,
+        None,
+    )
+    res = win32pipe.SetNamedPipeHandleState(
+        handle, win32pipe.PIPE_READMODE_BYTE, None, None
+    )
+    if res == 0:
+        print(f"SetNamedPipeHandleState return code: {res}")
     while True:
-        s.send(b"getsplitindex\r\n")
-        split_index = int(s.recv(1024).decode("utf-8"))
-        split_index_value.value = split_index
+        win32file.WriteFile(handle, b"getsplitindex\r\n")
+        result, data = win32file.ReadFile(handle, 65536)
+        if result == 0:
+            split_index_value.value = int(data.decode("utf-8").strip())
         time.sleep(LIVESPLIT_REQUEST_FREQUENCY)
 
 
@@ -50,24 +50,6 @@ class LivesplitClient:
         )
         self.process.daemon = True
         self.process.start()
-
-    def initialize_named_pipe(self):
-        # TODO this is preferred to tcp, but is currently not working for whatever reason.
-        self.handle = win32file.CreateFile(
-            r"\\.\pipe\LiveSplit",
-            win32file.GENERIC_READ | win32file.GENERIC_WRITE,
-            0,
-            None,
-            win32file.OPEN_EXISTING,
-            win32file.FILE_ATTRIBUTE_NORMAL,
-            None,
-        )
-        res = win32pipe.SetNamedPipeHandleState(
-            self.handle, win32pipe.PIPE_READMODE_BYTE, None, None
-        )
-        self.handle.write("startorsplit")
-        response = win32file.ReadFile(self.handle, 65536)
-        print(response)
 
     def tick(self):
         split_index = self.split_index_value.value
