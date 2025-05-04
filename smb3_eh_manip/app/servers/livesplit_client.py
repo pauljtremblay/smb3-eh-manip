5d1ad8ccdc747cb5e6e926044cfe41bb25fe64b4
Paul Tremblay <35076933+pauljtremblay@users.noreply.github.com>
2025-05-03 20:58:52 -0400

    chore: fiddle with named pipe calls

--- smb3_eh_manip/app/servers/livesplit_client.py
+++ smb3_eh_manip/app/servers/livesplit_client.py
@@ -11,6 +11,41 @@ LIVESPLIT_REQUEST_FREQUENCY = settings.get_float(
     "livesplit_request_frequency", fallback=0.25
 )
 
+NAMED_PIPE = r"\\.\pipe\LiveSplit"
+
+
+def create_named_pipe():
+    try:
+        return win32pipe.CreateNamedPipe(
+            NAMED_PIPE,
+            win32pipe.PIPE_ACCESS_DUPLEX,
+            win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_BYTE | win32pipe.PIPE_WAIT,
+            2,
+            65536,
+            65536,
+            0,
+            None
+        )
+    except:
+        LOGGER.error("Failed to create named pipe.", exc_info=True)
+        return None
+
+
+def get_pipe_file_handle():
+    try:
+        return win32file.CreateFile(
+            NAMED_PIPE,
+            win32file.GENERIC_READ | win32file.GENERIC_WRITE,
+            0,
+            None,
+            win32file.OPEN_EXISTING,
+            win32file.FILE_ATTRIBUTE_NORMAL,
+            None,
+        )
+    except:
+        LOGGER.error("Failed to get handle of named pipe.", exc_info=True)
+        return None
+
 
 def client_process(split_index_value: Value, do_split_value: Value):
     initialize_logging(
@@ -18,17 +53,10 @@ def client_process(split_index_value: Value, do_split_value: Value):
         file_log_level="DEBUG",
         filename="livesplit_client.log",
     )
-    handle = win32file.CreateFile(
-        r"\\.\pipe\LiveSplit",
-        win32file.GENERIC_READ | win32file.GENERIC_WRITE,
-        0,
-        None,
-        win32file.OPEN_EXISTING,
-        win32file.FILE_ATTRIBUTE_NORMAL,
-        None,
-    )
+    fd = create_named_pipe()
+    handle = get_pipe_file_handle()
     res = win32pipe.SetNamedPipeHandleState(
-        handle, win32pipe.PIPE_READMODE_BYTE, None, None
+         fd, win32pipe.PIPE_READMODE_BYTE, None, None
     )
     if res == 0:
         print(f"SetNamedPipeHandleState return code: {res}")
